82cc310e3d8b6d10b55a9820303208a317c5826a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-05-20 17:41:15 +0700

    app.css dan app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,6 @@
 const root = document.querySelector("#root");
+console.log("Hello");
+console.log("Fajar");
 
 function tick() {
   // const element=(
@@ -7,20 +9,30 @@ function tick() {
   //   <h2>{new Date().toLocaleTimeString()}</h2>
   //     </div>
   // );
-  const h1 = /*#__PURE__*/React.createElement("h1", null, "Jam Sekarang");
-  const h2 = /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString());
-
-  function klik() {
-    alert("OK!");
-  }
-
-  const button = /*#__PURE__*/React.createElement("button", {
-    onClick: klik
-  }, "Klik");
-  const element = React.createElement("div", {
-    className: "box",
-    children: [h1, h2, button]
-  });
+  // const h1 = (
+  //     <h1>Jam Sekarang</h1>
+  // );
+  // const h2 = (
+  //     <h2>{new Date().toLocaleTimeString()}</h2>
+  // );
+  // function klik() {
+  //     alert("OK!");
+  // }
+  // const button = (<button onClick={klik}>Klik</button>);
+  // const element = React.createElement("div", { className: "box", children: [h1, h2, button] })
+  const element = /*#__PURE__*/React.createElement("div", {
+    style: {
+      width: 200,
+      height: 200,
+      backgroundColor: 'salmon'
+    }
+  }, /*#__PURE__*/React.createElement("p", {
+    style: {
+      textAlign: 'center',
+      color: 'white',
+      lineHeight: '200px'
+    }
+  }, "Halo"));
   ReactDOM.render(element, root);
 }
 
