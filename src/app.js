6a795ced7994a5a70dc5c14428c40524eb102c87
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-05-20 17:44:16 +0700

    app.js, index.html, .gitignore

--- src/app.js
+++ src/app.js
@@ -1,25 +1,31 @@
-const root=document.querySelector("#root");
-function tick(){
+const root = document.querySelector("#root");
+console.log("Hello");
+console.log("Fajar");
+function tick() {
     // const element=(
     //     <div className="box">
-        //   <h1>Jam Sekarang</h1>
-        //   <h2>{new Date().toLocaleTimeString()}</h2>
+    //   <h1>Jam Sekarang</h1>
+    //   <h2>{new Date().toLocaleTimeString()}</h2>
     //     </div>
     // );
-    const h1=(
-        <h1>Jam Sekarang</h1> 
-    );
+    // const h1 = (
+    //     <h1>Jam Sekarang</h1>
+    // );
 
-    const h2=(
-        <h2>{new Date().toLocaleTimeString()}</h2>
-    );
-    
-    function klik(){
-        alert("OK!");
-    }
-    const button=(<button onClick={klik}>Klik</button>);
-    const element=React.createElement("div", {className:"box",  children:[h1,h2, button]})
+    // const h2 = (
+    //     <h2>{new Date().toLocaleTimeString()}</h2>
+    // );
 
+    // function klik() {
+    //     alert("OK!");
+    // }
+    // const button = (<button onClick={klik}>Klik</button>);
+    // const element = React.createElement("div", { className: "box", children: [h1, h2, button] })
+
+    const element =
+        (<div style={{ width: 200, height: 200, backgroundColor: 'salmon' }}>
+            <p style={{ textAlign: 'center', color: 'white', lineHeight: '200px' }}>Halo</p>
+        </div>);
     ReactDOM.render(element, root);
 }
 tick();
